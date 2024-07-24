da5739d4980645d9b0f85a6398426b0da37b0e22
rrekaF <pewuka2005@wp.pl>
2024-07-24 16:03:19 +0200

    Add printing guild id

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('GUILD_NAME')
 
 intents = discord.Intents.default()
 
@@ -14,5 +15,9 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            print(guild.id)
+            break
 
 client.run(TOKEN)
