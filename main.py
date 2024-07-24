b5a2e03306e3cf395c64d4474ed5627ef428f1b5
rrekaF <pewuka2005@wp.pl>
2024-07-24 17:05:12 +0200

    Added joining and leaving voice channels

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 
+import nacl
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('GUILD_NAME')
@@ -25,5 +27,23 @@ async def on_ready():
 @client.command(name='clip')
 async def info(ctx):
     await ctx.send("clip saved")
+
+
+@client.command(name='join')
+async def join(ctx):
+    if ctx.author.voice and ctx.author.voice.channel:
+        channel = ctx.author.voice.channel
+        await channel.connect()
+        await ctx.send(f"Joined {channel}")
+    else:
+        await ctx.send("You are not in a voice channel!")
+
+@client.command(name='leave')
+async def leave(ctx):
+    if ctx.voice_client:
+        await ctx.voice_client.disconnect()
+        await ctx.send("Disconnected from the voice channel!")
+    else:
+        await ctx.send("I am not in a voice channel!")
 
 client.run(TOKEN)
