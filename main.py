e31c00cc2beee482af16d87f236ab4ff80530197
rrekaF <pewuka2005@wp.pl>
2024-07-25 12:25:45 +0200

    Add audio recording to file

--- main.py
+++ main.py
@@ -1,44 +1,34 @@
-# bot.py
 import os
-
 import discord
-from dotenv import load_dotenv
 from discord.ext import commands
+from discord.ext.voice_recv import VoiceRecvClient, BasicSink, VoiceData
+from dotenv import load_dotenv
+import wave
+import audioop
 
-import nacl
-
+# Load environment variables
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('GUILD_NAME')
 
+# Set up intents
 intents = discord.Intents.default()
 intents.message_content = True
+intents.voice_states = True
+intents.guilds = True
 
-client = commands.Bot(command_prefix='!', intents=intents)
+# Initialize bot
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            print(guild.id)
-            break
-
-@client.command(name='clip')
-async def info(ctx):
-    await ctx.send("clip saved")
-
-
-@client.command(name='join')
+@bot.command(name='join')
 async def join(ctx):
     if ctx.author.voice and ctx.author.voice.channel:
         channel = ctx.author.voice.channel
-        await channel.connect()
+        await channel.connect(cls=VoiceRecvClient)
         await ctx.send(f"Joined {channel}")
     else:
         await ctx.send("You are not in a voice channel!")
 
-@client.command(name='leave')
+@bot.command(name='leave')
 async def leave(ctx):
     if ctx.voice_client:
         await ctx.voice_client.disconnect()
@@ -46,4 +36,52 @@ async def leave(ctx):
     else:
         await ctx.send("I am not in a voice channel!")
 
-client.run(TOKEN)
+audio_packets = []
+
+@bot.command(name='start_capture')
+async def start_capture(ctx):
+    global audio_packets
+    if ctx.voice_client:
+        audio_packets = []  # Reset the list
+        def callback(user, data: VoiceData):
+            if data.opus is not None:
+                audio_packets.append(data.pcm)
+
+        vc = ctx.voice_client
+        vc.listen(BasicSink(callback))
+        await ctx.send("Started capturing audio!")
+    else:
+        await ctx.send("I am not in a voice channel!")
+
+@bot.command(name='stop_capture')
+async def stop_capture(ctx):
+    global audio_packets
+    if ctx.voice_client:
+        ctx.voice_client.stop()
+        await ctx.send("Stopped capturing audio!")
+        if audio_packets:
+                    with wave.open(f'recording_{ctx.guild.id}.wav', 'wb') as wf:
+                        wf.setnchannels(2)
+                        wf.setsampwidth(2)
+                        wf.setframerate(48000)
+                        for audio in audio_packets:
+                            wf.writeframes(audioop.mul(audio, 2, 1))
+
+
+@bot.command(name='die')
+async def die(ctx):
+    if ctx.voice_client:
+        ctx.voice_client.stop()
+        await ctx.voice_client.disconnect()
+    await ctx.bot.close()
+
+@bot.event
+async def on_ready():
+    if bot.user != None:
+        print(f'Logged in as {bot.user.id}/{bot.user.name}')
+        print('------')
+
+if TOKEN != None:
+    bot.run(TOKEN)
+else:
+    print("Token is None")
